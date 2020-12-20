c7172c2919b6c52c93134c3a8d108bd612fc451a
Sergey Schukin <schukin@reksoft.ru>
2020-12-20 14:27:11 +0300

    code.cleanup: some fixes

--- src/main/java/com/test/rest/dto/AtmosphericInformationDto.java
+++ src/main/java/com/test/rest/dto/AtmosphericInformationDto.java
@@ -1,9 +1,5 @@
 package com.test.rest.dto;
 
-import com.test.exception.WeatherException;
-import com.test.model.PointData;
-import com.test.model.PointDataType;
-import com.test.util.Constants;
 import lombok.*;
 
 import java.util.HashMap;
@@ -17,7 +13,7 @@ import java.util.Map;
 @NoArgsConstructor
 public class AtmosphericInformationDto {
 
-    private Map<PointDataType, PointData> information = new HashMap<>();
+    private Map<PointDataTypeDto, PointDataDto> information = new HashMap<>();
 
     /**
      * the last time this data was updated, in milliseconds since UTC epoch
